058c82d721fd76def6ec0a8338be2b154acf2fe9
anton4o5 <anton.kapanov@gmail.com>
2016-03-30 01:11:48 +0300

    Adding(Point.java, Projectiles.java)

---
+++ Zombie-Land/src/Utilities/Point.java
@@ -0,0 +1,12 @@
+public class Point{
+    public int x;
+    public int y;
+
+    public Point(int x, int y){
+
+            this.x = x;
+            this.y = y;
+
+    }
+
+}
---
+++ Zombie-Land/src/Utilities/Projectiles.java
@@ -0,0 +1,13 @@
+
+public class Projectiles {
+
+
+    public double Cosine_A(Point pointA, Point pointB){
+        int sideA = pointA.x - pointB.x;
+        int sideB = pointA.y - pointB.y;
+        double hypotenuse = Math.sqrt(Math.pow(sideA,2) + Math.pow(sideB,2));
+
+        double cosine = sideA / hypotenuse;
+        return cosine;
+    }
+}
